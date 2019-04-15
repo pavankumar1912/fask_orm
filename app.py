79a9089aa24878f823c4955c5d21b14196392df3
pavankumar1912 <pavan.polepalli@gmail.com>
2019-04-15 17:01:09 +0530

    table creation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "inventory.db"))
@@ -10,6 +11,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 api = Api(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+from models import Employee,User_role
 #from flask_sqlalchemy import SQLAlchemy
 
 @app.route('/')
--- models.py
+++ models.py
@@ -1,1 +1,28 @@
 # Create models here
+from app import db
+
+class User_role(db.Model):
+    role_id = db.Column(db.Integer,nullable=False,primary_key=True,unique= True)
+    role_name = db.Column(db.String(100), nullable=True)
+    employes = db.relationship('Employee',backref = 'user_role', lazy = 'dynamic')
+    def __repr__(self):
+        return (self.role_name)
+
+
+class Employee(db.Model):
+    employeeId = db.Column(db.Integer, primary_key=True)
+    employeeName = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(100),nullable=False, unique=True)
+    joiningDate = db.Column(db.String,nullable = False)
+    password = db.Column(db.String(100),nullable= False)
+    role_id = db.Column(db.Integer,db.ForeignKey('user_role.role_id'))
+    gender = db.Column(db.String(100),nullable= False)
+    
+    def __repr__(self):
+        return self.employeeName
+
+
+
+    
+
+    
