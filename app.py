c14a35cff63e14c0879e37d7a1469512ba2f42a5
kiran <kennysmart2407@gmail.com>
2019-04-19 10:26:16 +0530

    updated

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, request
+from flask import Flask, request,jsonify
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "inventory.db"))
@@ -11,14 +11,39 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 api = Api(app)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-from models import Employee,User_role
-#from flask_sqlalchemy import SQLAlchemy
+
+
+import models
+from models import *
+
 
 @app.route('/')
 #@login_required
 def hello():
-	return '<h1>Welcome to Inventory Application</h1>'
+    a = User_role.query.all()
+    print(a)
+    return '<h1>Welcome to Inventory Application</h1>'
+@app.route("/user", methods=["GET"])
+def get_user():
+    print('2nd call')
+    all_users = User_role.query.all()
+    print(all_users)
+    #result = users_schema.dump(all_users)
+    return "Hello World"
+
+@app.route("/user", methods=["POST"])
+def add_user():
+    print("post call")
+    rid = request.json['username']
+    rname = request.json['email']
+    
+    new_user = User_role(role_id =rid,role_name=rname)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return "post call success"
+
 
 if __name__ == '__main__':
 
